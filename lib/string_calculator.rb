2d0ae19f9cb092259067622bf55e3716fa46c18a
Ricardo Sandoval <ricardo.sandoval@apptegy.com>
2022-12-05 15:49:52 -0600

    Final solution for the string calculator kata

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,11 +1,41 @@
-# frozen_string_literal: true
-
 class StringCalculator
-  def add(string)
-    return 0 if string.empty?
 
-    number_to_add = string.split(/[^\d]/).map { |num| num.to_i }
-    number_to_add.sum
+  attr_accessor :called_count, :positives, :negatives
+  LIMIT = 1_000.freeze
+  REGEX = /[^-?\d+(\.\d+)?$]/.freeze
+  def initialize
+    @numbers = Hash.new
+    @positives = Array.new
+    @negatives = Array.new
+    @called_count = 0
   end
 
+  def add(string)
+    increase_called_count
+    return 0 if string.strip.empty?
+    map_numbers(string)
+    get_negatives
+    @positives.sum
+    #@positives.sum
+
+  end
+
+  def increase_called_count
+    @called_count = @called_count + 1
+  end
+
+  def get_negatives
+    raise ArgumentError.new("A negative number cannot be sent in the string.") if @negatives.count == 1
+    raise ArgumentError.new("Multiple negative numbers have been detected: #{@negatives.join(',')}") if @negatives.count > 1
+    @negatives
+  end
+
+  private
+  def map_numbers(string)
+    @numbers = string.split(REGEX).map do |number|
+      number.to_i < LIMIT ? number.to_i : 0
+    end.group_by { |n| n.negative? ? :negative : :positive }
+
+    @positives, @negatives = @numbers[:positive].to_a, @numbers[:negative].to_a
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,47 +2,68 @@ require 'string_calculator'
 
 describe StringCalculator do
 
+  subject { described_class.new }
+
   it "returns zero when the string is empty" do
-    calculator = StringCalculator.new
-    result = calculator.add("")
-    expect(result).to eq(0)
+    expect(subject.add("")).to eq(0)
   end
 
   it "returns the same number when is one number" do
-    calculator = StringCalculator.new
-    result = calculator.add("1")
-    expect(result).to eq(1)
+    expect(subject.add("1")).to eq(1)
+    expect(subject.add("5")).to eq(5)
+    expect(subject.add("312")).to eq(312)
   end
 
   it "returns the sum of two number separated by a comma" do
-    calculator = StringCalculator.new
-    result = calculator.add("1,2")
-    expect(result).to eq(3)
+    expect(subject.add("1,2")).to eq(3)
+    expect(subject.add("1,3")).to eq(4)
+    expect(subject.add("1,0")).to eq(1)
   end
 
   it "returns the sum of an unknown amount of numbers" do
-    calculator = StringCalculator.new
-    result = calculator.add("1,2,3,4,5,6,7,8,9,10")
-    expect(result).to eq(55)
+    expect(subject.add("1,2,3,4,5,6,7,8,9,10")).to eq(55)
+    expect(subject.add("22,23,1,0,0")).to eq(46)
+    expect(subject.add("1,2,3,4,5")).to eq(15)
   end
 
   it "returns the sum of two number separated by a new line" do
-    calculator = StringCalculator.new
-    result = calculator.add("1\n2,3")
-    expect(result).to eq(6)
+    expect(subject.add("1\n2,3")).to eq(6)
+    expect(subject.add("1\n3,4")).to eq(8)
+    expect(subject.add("1,3\n4")).to eq(8)
   end
 
   it "contains comma delimiters at the beginning, in the middle or the end of the string" do
-    calculator = StringCalculator.new
-    result = calculator.add("//;\n1;2")
-    expect(result).to eq(3)
+    expect(subject.add(",1,2,")).to eq(3)
+    expect(subject.add(",4,2,")).to eq(6)
+    expect(subject.add(",1,2,3,")).to eq(6)
   end
 
-  it "returns the sum of contains delimiters, different of the comma, at the beginning, in the middle or the end of the string" do
-    calculator = StringCalculator.new
-    result = calculator.add("//;\n1;2")
-    expect(result).to eq(3)
+  it "returns the sum with delimiters, different of the comma, at the beginning, in the middle or the end of the string" do
+    expect(subject.add("//;\n1;2")).to eq(3)
+    expect(subject.add("//;$\n1!;2,3")).to eq(6)
   end
 
+  it "raises ArgumentError when a negative number is sent in the string" do
+    expect { subject.add("-1") }.to raise_error(ArgumentError, "A negative number cannot be sent in the string.")
+  end
+
+  it "raises ArgumentError when a negative number is sent in the string" do
+    expect { subject.add("-1,-2,-3") }.to raise_error(ArgumentError, "Multiple negative numbers have been detected: -1,-2,-3")
+  end
+
+  it "returns the number of calls made to the add method" do # Why the called_count is not being modified, after multiple calls to the add method?
+    subject.add("//;\n1;2")
+    expect(subject.called_count).to eq(1)
+    subject.add("//;\n1;2")
+    expect(subject.called_count).to eq(2)
+  end
+
+  it "omits numbers bigger than a 1000" do
+    expect(subject.add("2,1001")).to eq(2)
+  end
+
+  it "returns the sum of the numbers no matter the length of the delimiters" do
+    expect(subject.add("//[***]\n1***2***3")).to eq(6)
+  end
 end
 
